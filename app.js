5890e62a72ef54c36910de9565f093426000e527
demi0504 <ripley.demi@gmail.com>
2020-08-01 14:58:05 -0500

    created add employee function

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
+//department, role, employee, manager arrays to be filled when added too and updated
+let deptArr = [];
+let roleArr = [];
+let emplArr = [];
+let managerArr = [];
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -117,7 +122,53 @@ function init() {
             connection.end();
         };
     });
+    //update arrays each time init function is called
+    getDepts();
+    getRoles();
+    getManagers();
 };
+
+// Get all departments
+function getDepts() {
+  connection.query(`SELECT department_name FROM department`, function (
+    err,
+    departments
+  ) {
+    if (err) throw err;
+    deptArr = [];
+    for (i = 0; i < departments.length; i++) {
+      deptArr.push(departments[i].department_name);
+    }
+    // console.log(deptArr);
+  });
+}
+
+// Get all roles
+function getRoles() {
+  connection.query(`SELECT title FROM role`, function (err, roles) {
+    if (err) throw err;
+    roleArr = [];
+    for (i = 0; i < roles.length; i++) {
+      roleArr.push(roles[i].title);
+    }
+    // console.log(roleArr);
+  });
+}
+
+// Get all potential managers by last name
+function getManagers() {
+  connection.query(`SELECT employee.last_name FROM employee`, function (
+    err,
+    managers
+  ) {
+    if (err) throw err;
+    emplArr = [];
+    for (i = 0; i < managers.length; i++) {
+      managerArr.push(managers[i].last_name);
+    }
+    // console.log(managerArr);
+  });
+}
 
 //Manager prompt
 function promptManagers (managers) {
@@ -152,6 +203,69 @@ function queryEmployeesAll(){
   );
 }
 
+//add employee
+function addEmployee() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    connection.query("SELECT * FROM employee", function (err, res2) {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?",
+          },
+          {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?",
+          },
+          {
+            name: "roleName",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: roleArr,
+          },
+          {
+            name: "managerName",
+            type: "list",
+            message: "Who is this employee's Manager?",
+            choices: managerArr,
+          },
+        ])
+        .then(function (answer) {
+          let roleID;
+          for (let r = 0; r < res.length; r++) {
+            if (res[r].title == answer.roleName) {
+              roleID = res[r].role_id;
+            }
+          }
+          let managerID;
+          for (let m = 0; m < res2.length; m++) {
+            if (res2[m].last_name == answer.managerName) {
+              managerID = res2[m].employee_id;
+            }
+          }
+          // when finished prompting, insert a new item into the db with that info
+          connection.query(
+            "INSERT INTO employee SET ?",
+            {
+              first_name: answer.first_name,
+              last_name: answer.last_name,
+              role_id: roleID,
+              manager_id: managerID,
+            },
+            function (err) {
+              if (err) throw err;
+            }
+          );
+          init();
+        });
+    });
+  });
+}
+
 //query all departments
 function viewDepartments() {
     const query = `SELECT department.department_name FROM department`;
@@ -168,20 +282,6 @@ function viewDepartments() {
     });
 };
 
-// function queryDepartmentsCallBack(callback){
-//     const query = `SELECT department.name FROM department;`;
-//     connection.query(query, (err, res) => {
-//         if (err) throw err;
-//         //extract department names to array
-//         const departments = [];
-//         for (let i = 0; i < res.length; i++) {
-//             departments.push(res[i].name);
-//         }
-//         //prompt for department selection
-//        callback(departments)
-//     });
-// }
-
 //query roles and display
 function viewRoles() {
   connection.query(`SELECT * FROM role`, function (err, data) {
@@ -190,8 +290,7 @@ function viewRoles() {
     init();
   });
 }
-//query all managers
-// function 
+
 //query employees by department
 function viewByDepartment(department) {
     const query = `
