a82466994ecab7963d634e23b6822e2a5ca8f9ab
demi0504 <ripley.demi@gmail.com>
2020-08-01 08:47:14 -0500

    refactoring seeds.sql

--- employee-schemas.sql
+++ employee-schemas.sql
@@ -4,23 +4,26 @@ CREATE database employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (department_id)
 );
 
 CREATE TABLE role (
-  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  role_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,4) NOT NULL,
-  department_id INT NOT NULL
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (role_id)
 );
 
 CREATE TABLE employee (
-  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  employee_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NULL
+  manager_id INT NOT NULL,
+  PRIMARY KEY (employee_id)
 );
 
 
